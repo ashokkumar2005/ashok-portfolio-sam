a2757eab3d48a0c75b9d1b5c8017fe0d6f2966a7
Ashokkumar <136552833+ashokkumar2005@users.noreply.github.com>
2026-03-17 13:47:33 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,13 @@
 const API = import.meta.env.VITE_API_URL;
+
+// ✅ USERS
 export const getUsers = async () => {
-    const res = await fetch(`${API}/api/users`);
-    return res.json();
+  const res = await fetch(`${API}/api/users`);
+  return res.json();
+};
+
+// ✅ PROJECTS
+export const getProjects = async () => {
+  const res = await fetch(`${API}/api/projects`);
+  return res.json();
 };
